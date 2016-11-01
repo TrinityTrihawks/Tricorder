995fc1bbe06a9e635bf3212f0c0467b881aab143
Waweros <lion@waposta.com>
2016-11-01 00:02:59 -0500

    First Commit

---
+++ src/main/java/frc/team4215/tricorder/Tricorder.java
@@ -0,0 +1,23 @@
+package frc.team4215.tricorder;
+
+import jaci.openrio.toast.lib.module.ToastModule;
+
+public class Tricorder extends ToastModule {
+	
+	@Override
+	public String getModuleName() {
+		return "Tricorder";
+	}
+
+	@Override
+    public String getModuleVersion() {
+        return "0.0.1";
+    }
+
+	@Override
+	public void prestart(){}
+
+	@Override
+	public void start(){}
+
+}
